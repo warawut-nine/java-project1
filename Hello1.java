ad7eeb448839e6f79a90235e9b9b663b6e5e8321
unknown <6910122113031@pnru.ac.th>
2026-09-18 11:07:24 +0700

    Update Message

--- Hello1.java
+++ Hello1.java
@@ -1,8 +1,6 @@
 public class Hello1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello1");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
+        System.out.println("I love PNRU");
     }
 }
