433958a9bf679005dfb395f6d58b20224c8f4ebd
unknown <6910122113031@pnru.ac.th>
2026-09-18 11:01:57 +0700

    My Editting-2

--- Hello1.java
+++ Hello1.java
@@ -1,8 +1,8 @@
 public class Hello1 {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
